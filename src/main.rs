0257f2a647d8f29812564f53be826d0079161feb
Kirill <kirill.shlenskiy@gmail.com>
2019-02-08 23:43:30 +1100

    Work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::error::*;
-use std::net::{ToSocketAddrs,TcpStream};
+use std::net::{SocketAddr,ToSocketAddrs,TcpStream};
 use std::time::Duration;
 
 use clap::{App,AppSettings,Arg};
@@ -10,10 +10,23 @@ fn main() {
         .version("0.1")
         .about("TCP ping utility by Kirill Shlenskiy (2019)")
         .arg(Arg::from_usage("<target> 'TCP ping target in {host:port} format (i.e. google.com:80)'"))
+        .arg(Arg::from_usage("-c, --count 'Number of requests (not counting warmup) to issue"))
         .arg(Arg::from_usage("-t, --timeout 'Connection timeout in milliseconds'"))
         .setting(AppSettings::ArgRequiredElseHelp)
         .setting(AppSettings::DisableVersion)
         .get_matches();
+
+    let count_match = matches.value_of("count");
+    let mut count = 4; // Default.
+
+    if let Some(count_str) = count_match {
+        count = count_str.parse::<i32>().expect("Not a valid integer.");
+
+        if count < 0 {
+            println!("Invalid count.");
+            return;
+        }
+    }
 
     let target = matches.value_of("target").unwrap();
     let socket_addr_result = target.to_socket_addrs();
@@ -25,23 +38,35 @@ fn main() {
     }
 
     let addr = socket_addr_result.unwrap().next().unwrap();
-    println!("Connecting to {}", addr);
 
-    // Connect.
+    // Warmup.
+    print!("Connecting to {} (warmup): ", addr);
+    ping(&addr);
+
+    // Actual ping.
+    for i in 0..count {
+        print!("Connecting to {}: ", addr);
+        ping(&addr);
+    }
+}
+
+fn ping(addr: &SocketAddr) {
     let res = TcpStream::connect_timeout(&addr, Duration::from_millis(5000));
 
     if let Err(err) = res {
+        println!(); // Complete current line.
         println!("{} {}", style("Can't connect.").red(), style(err).red());
         return;
     }
 
-    println!("{}", style("Success!").green());
+    println!("{}", style("success!").green());
 }
 
 fn fmt_err(err: &Error) -> String {
     let mut desc = Vec::new();
     {
-        let mut desc_chars = err.description().chars();
+        let desc_orig = format!("{}", err);
+        let mut desc_chars = desc_orig.chars();
 
         while let Some(c) = desc_chars.next() {
             // Capitalise first letter.
