6f8db011e7c4171e76ba0b5ec5a68584a2ffa700
Kirill <kirill.shlenskiy@gmail.com>
2019-02-10 21:09:39 +1100

    Version change (v0.4.1)

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ mod aggregates;
 
 fn main() -> Result<(), Box<Error>> {
     let matches = App::new("tcping")
-        .version("0.4")
+        .version("0.4.1")
         .about("TCP ping utility by Kirill Shlenskiy (2019)")
         .arg(Arg::from_usage("<target> 'TCP ping target in {host:port} format (i.e. google.com:80)'"))
         .arg(Arg::from_usage("-c, --count=[count] 'Number of requests (not counting warmup) to dispatch'"))
