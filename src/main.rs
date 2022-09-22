5a0f4789014033a3eff5ab5be5f49d43ac8ddfb9
Kirill Shlenskiy <kirill.shlenskiy@gmail.com>
2022-09-22 12:25:28 +1000

    Bump version (v0.9.7)

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ async fn main() {
 
 async fn main_impl() -> Result<(), Box<dyn Error>> {
     let matches = App::new("tcping")
-        .version("0.9.6")
+        .version("0.9.7")
         .about("TCP ping utility by Kirill Shlenskiy (2022)")
         .arg(Arg::from_usage("<target> 'TCP ping target in \"host:port\" format (i.e. google.com:80)'"))
         .arg(Arg::from_usage("-t 'Ping until stopped with Ctrl+C'"))
